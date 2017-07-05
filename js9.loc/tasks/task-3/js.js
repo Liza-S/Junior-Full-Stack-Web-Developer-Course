32de5206e80b40e7979ac80f7f757972e8157edd
Liza :) <lizash605@.gmail.com>
2017-07-06 00:18:42 +0300

    Added js9

---
+++ js9.loc/tasks/task-3/js.js
@@ -0,0 +1,6 @@
+var name1 = 'Лена';
+var name2 = 'Лен';
+var name3 = 'Леа';
+var name4 = 'Ла';
+var name5 = 'Олег';
+console.log(name1, name2, name3, name4, name5);
---
+++ js9.loc/tasks/task-4/js.js
@@ -0,0 +1,11 @@
+var stringObject = new String('эта строка - объект');
+var string = String('просто строка');
+var string2 = 'еще одна строка';
+
+console.log(typeof stringObject);
+console.log(typeof string);
+console.log(typeof string2);
+
+console.log(stringObject);
+console.log(string);
+console.log(string2);
